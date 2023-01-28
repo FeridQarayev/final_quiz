5d2b46b99e3e6a46e8d03633a8cf7c6417695e71
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-28 15:39:13 +0400

    add home html

---
+++ client/src/pages/home/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "./home.module.scss";
+
+function Home() {
+  return <div className={styled.home}>
+    <div className={styled.home__container}>
+      <div className={styled.home__container__up}>
+        <div className={styled.home__container__up__left}></div>
+        <div className={styled.home__container__up__right}>
+          <div className={styled.home__container__up__right__text}>
+            <div className={styled.home__container__up__right__text__title}>
+              <p className={styled.home__container__up__right__text__title__shop}>Shop is fun</p>
+              <p className={styled.home__container__up__right__text__title__browse}>BROWSE OUR PREMIUM PRODUCT</p>
+            </div>
+            <div className={styled.home__container__up__right__text__down}>
+              <p>Us which over of signs divide dominion deep fill bring they're meat beho upon own earth without morning over third. Their male dry. They are great appear whose land fly grass.</p>
+              <button>Browse Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
+}
+
+export default Home;
