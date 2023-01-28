b2247d0246bc9fbaea3e9e90b7bea832d7f79771
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-28 16:01:15 +0400

    add home page up section styled

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -6,11 +6,14 @@ function Home() {
     <div className={styled.home__container}>
       <div className={styled.home__container__up}>
         <div className={styled.home__container__up__left}></div>
+        <div className={styled.home__container__up__img}>
+          <img src="https://preview.colorlib.com/theme/aroma/img/home/hero-banner.png" alt="basket" />
+        </div>
         <div className={styled.home__container__up__right}>
           <div className={styled.home__container__up__right__text}>
             <div className={styled.home__container__up__right__text__title}>
               <p className={styled.home__container__up__right__text__title__shop}>Shop is fun</p>
-              <p className={styled.home__container__up__right__text__title__browse}>BROWSE OUR PREMIUM PRODUCT</p>
+              <h1 className={styled.home__container__up__right__text__title__browse}>BROWSE OUR PREMIUM PRODUCT</h1>
             </div>
             <div className={styled.home__container__up__right__text__down}>
               <p>Us which over of signs divide dominion deep fill bring they're meat beho upon own earth without morning over third. Their male dry. They are great appear whose land fly grass.</p>
